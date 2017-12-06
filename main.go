b4cf843d2254f52093d019da435fc4e6f919d9bc
Russ Smith <rsmith@newrelic.com>
2017-12-06 01:44:17 -0800

    First commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+
+	"github.com/gumper23/sql/qm"
+)
+
+func main() {
+	fmt.Printf("Howdy\n")
+
+	dsn, ok := os.LookupEnv("POSTGRES_DSN")
+	if ok {
+		db, err := sql.Open("postgres", dsn)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		err = db.Ping()
+		if err != nil {
+			panic(err)
+		}
+
+		var cols []string
+		rows, cols, err := qm.QueryRows(db, "select * from pg_stat_activity")
+		if err != nil {
+			panic(err)
+		}
+		printQueryMap(rows, cols)
+	}
+
+	dsn, ok = os.LookupEnv("MYSQL_DSN")
+	if ok {
+		db, err := sql.Open("mysql", dsn)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		err = db.Ping()
+		if err != nil {
+			panic(err)
+		}
+
+		rows, cols, err := qm.QueryRows(db, "select * from information_schema.processlist")
+		if err != nil {
+			panic(err)
+		}
+		printQueryMap(rows, cols)
+	}
+}
+
+func printQueryMap(qm []map[string]string, cols []string) {
+	for _, row := range qm {
+		for _, col := range cols {
+			fmt.Printf("%-20s\t%s\n", col+":", row[col])
+		}
+		fmt.Println()
+	}
+}
